a51f05c5acc2095c95a6b999d1d5772209d7b112
Александр <alexudalovvvvv@outlook.com>
2018-02-17 17:58:07 +0300

    DateRegex

---
+++ TRPO/TRPO/DateInformer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateRegex
+{
+    static class DateInformer
+    {
+        public static DateTime GetNextDay(DateTime current) => 
+            new DateTime(current.Year, current.Month, current.Day).AddDays(1);
+        
+        public static DateTime GetNextMonth(DateTime current) => 
+            new DateTime(current.Year, current.Month, 1).AddMonths(1);
+
+        public static DateTime GetCurrentWeekBegining(DateTime current) =>
+            new DateTime(current.Year, current.Month, current.Day).AddDays( - (int)current.DayOfWeek);
+
+        public static DateTime GetCurrentWeekEnding(DateTime current) =>
+            new DateTime(current.Year, current.Month, current.Day).AddDays(7 - (int)current.DayOfWeek).AddSeconds(-1);
+
+        
+    }
+}
--- TRPO/TRPO/Program.cs
+++ TRPO/TRPO/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Globalization;
 
 namespace DateRegex
 {
     class Program
     {
+        public const string Format = "yyyy-MM-ddTHH:mm:sszz";
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter date in ISO format:");
+            
+            try
+            {
+                DateTime inputDate = DateTime.ParseExact(Console.ReadLine(), Format, CultureInfo.InvariantCulture);
+
+                Console.WriteLine("Next day date: " + DateInformer.GetNextDay(inputDate).ToString(Format));
+                Console.WriteLine("Current week beggining date: " + DateInformer.GetCurrentWeekBegining(inputDate).ToString(Format));
+                Console.WriteLine("Current week ending date: " + DateInformer.GetCurrentWeekEnding(inputDate).ToString(Format));
+                Console.WriteLine("Next month date: " + DateInformer.GetNextMonth(inputDate).ToString(Format));
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Incorrect format: " + fe.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
