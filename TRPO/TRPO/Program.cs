9e2c3c874a4218a92299e9ff0a6851e7359d471b
Александр <alexudalovvvvv@outlook.com>
2018-02-17 12:08:51 +0300

    Init solution and project

---
+++ TRPO/TRPO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DateRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
