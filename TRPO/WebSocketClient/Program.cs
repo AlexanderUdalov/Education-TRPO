65b12e87c4f11d7264ee73db6600f4e8f3a7f928
Александр <alexudalovvvvv@outlook.com>
2018-03-20 08:30:47 +0300

    Added websocket service.

---
+++ TRPO/WebSocketClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketClient
+{
+    public class Program
+    {
+        public static void Main(string[] args) => RunWebSocketsAsync().GetAwaiter().GetResult();
+
+        private static async Task RunWebSocketsAsync()
+        {
+            var client = new ClientWebSocket();
+            await client.ConnectAsync(new Uri("ws://localhost:55555/"), CancellationToken.None);
+
+            Console.WriteLine("Connected!");
+
+            await ReceivingAsync(client);
+        }
+
+        private static async Task ReceivingAsync(ClientWebSocket client)
+        {
+            var buffer = new byte[1024 * 4];
+            
+            while (true)
+            {
+                var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Text)
+                    Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, result.Count));
+
+                else if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ TRPO/WebSocketServer/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketServer
+{
+    
+    public class Program
+    {
+        public static void Main(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build().Run();
+    }
+
+    public class Startup
+    {
+        public static string PreviousValue;
+        public static HttpClient client = new HttpClient();
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseWebSockets();
+            app.Use(async (context, next) =>
+            {
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    await Function(context, webSocket);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                }
+            });
+        }
+        
+        private async Task Function(HttpContext context, WebSocket webSocket)
+        {
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var currentValue = await client.GetStringAsync(@"https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD");
+                await SendMessageAsync(webSocket, "Trying get values...");
+                if (!currentValue.Equals(PreviousValue))
+                {
+                    PreviousValue = currentValue;
+                    await SendMessageAsync(webSocket, currentValue.ToString());
+                }
+                await Task.Delay(3000);
+            }
+            await webSocket.CloseAsync(closeStatus: WebSocketCloseStatus.NormalClosure,
+                                    statusDescription: "Closed succesfull",
+                                    cancellationToken: CancellationToken.None);
+        }
+
+        public async Task SendMessageAsync(WebSocket socket, string message)
+        {
+            if (socket.State != WebSocketState.Open)
+                return;
+
+            await socket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.ASCII.GetBytes(message),
+                                                                  offset: 0,
+                                                                  count: message.Length),
+                                   messageType: WebSocketMessageType.Text,
+                                   endOfMessage: true,
+                                   cancellationToken: CancellationToken.None);
+        }
+    }
+}
